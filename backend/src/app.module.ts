1134734f8dbc392fa02a5c86b2831b7ed309d214
Meiado <gabrielmeiado@hotmail.com>
2024-09-10 18:22:06 -0300

    adicionado disparo de email via mailguner

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,18 @@ import { DoacoesModule } from './doacoes/doacoes.module';
 import { ProdutosModule } from './produtos/produtos.module';
 import { CidadesModule } from './cidades/cidades.module';
 import { EstadosModule } from './estados/estados.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
+    MailerModule.forRoot({
+      host: 'smtp.mailgun.org',
+      secure: false,
+      port: 587,
+      auth: {
+        user: ''
+      }
+    }),
     PessoasModule,
     PrismaModule,
     DoacoesModule,
--- backend/src/pessoas/observer/doador.observer.ts
+++ backend/src/pessoas/observer/doador.observer.ts
@@ -1,30 +1,29 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Observer } from 'src/utils/interfaces/Observer';
 
 export class DoadorObserver implements Observer {
   notify(eventType: string, data: any): void {
     if (eventType === 'DOACAO_RECEBIDA' && data.pes_id_doador === this.pes_id) {
-      console.log(
-        'Confirmação de recebimento de doação do doador ' +
-          this.pes_email +
-          ' (' +
-          this.pes_id +
-          ')',
-      );
+      this.mailerService.sendMail({
+        to: this.pes_email,
+        subject: 'Sua doação foi recebida',
+        html: `<h3>Sua doação foi recebida, agradecemos por contribuir. Se por algum acaso não foi feita por você, por favor entre em contato.</h3>`,
+      });
     }
     if (
       eventType === 'DOACAO_CANCELADA' &&
       data.pes_id_doador === this.pes_id
     ) {
-      console.log(
-        'Sua doação foi cancelada, favor entrar em contato para conferir. Email doador: ' +
-          this.pes_email +
-          ', ID: ' +
-          this.pes_id,
-      );
+      this.mailerService.sendMail({
+        to: this.pes_email,
+        subject: 'Sua doação foi cancelada',
+        html: `<h3>Sua doação foi cancelada, entre em contato para mais informações</h3>`,
+      });
     }
   }
 
   constructor(
+    private mailerService: MailerService,
     public pes_id: number,
     public pes_email: string,
   ) {}
